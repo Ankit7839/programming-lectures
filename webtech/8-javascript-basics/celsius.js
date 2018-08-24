260f50f4e6263ec4e6362edf1095a4140f4f8dd4
git_chetan <chetanpandit2011@gmail.com>
2018-08-24 09:51:04 +0530

    Taking input from command line.

---
+++ webtech/8-javascript-basics/celsius.js
@@ -0,0 +1,22 @@
+// We are writing a program to convert celcius to Farenhite.
+
+// Initialize celsius variable here
+let readline = require('readline-sync'); // this readline sync is a good module to start with.
+
+let celsius;
+
+celsius = readline.question();
+
+// write the equation to convert celsius to farenhite
+
+// Tf = Tc * (9/5) + 32
+
+// convert it to program to pass it to your computer.
+
+let farenhite;
+
+farenhite = celsius * (9/5) + 32;
+
+// print result to console.
+
+console.log(celsius + " C is equal to "+ farenhite + " F");
