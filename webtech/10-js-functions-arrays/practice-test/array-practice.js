b39b8d201a005d55bc727d5c187ef549cb15fc91
git_chetan <chetanpandit2011@gmail.com>
2018-08-30 23:25:17 +0530

    practice set Array JS.

--- webtech/10-js-functions-arrays/practice-test/array-practice.js
+++ webtech/10-js-functions-arrays/practice-test/array-practice.js
@@ -1,22 +1,12 @@
 // Let's initialize an array consisting the names of family members
-let family = ["MCA", "Civil", "CS", "Mech", "KSOP"];
 
 // print all the values of array
-console.log(family);
-
 
 // print value from a specific index , from index one
-console.log(family[1]);
 
 //print length of array
-console.log(family.length);
 
 //print last element in array
-console.log(family[family.length-1]);
 
 //print all the values of array one by one
-for(i=0; i<family.length; i++){
-    console.log(family[i]);
-    
-}
 
