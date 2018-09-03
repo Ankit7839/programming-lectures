017f10c1c8aef49bc52cf424dff2a85c2fbfc627
git_chetan <chetanpandit2011@gmail.com>
2018-09-04 00:48:58 +0530

    Call back functions.

---
+++ webtech/12-callback-functions/one.js
@@ -0,0 +1,16 @@
+const todo =[];
+todo.push("Make youtube video");
+todo.push("Push JS code on Github");
+todo.push("Next ML project to generate music");
+
+todo.forEach(function(todoItem, index, index1) {
+    console.log(`Todo item at ${index} is ${todoItem}`);
+    
+});
+
+//using arrow funciton
+
+todo.forEach(element => {
+console.log(element);
+    
+});
