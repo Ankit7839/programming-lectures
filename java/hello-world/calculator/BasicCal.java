6f78551824ef742b7bc37750a112603cbf751f4e
git_chetan <chetanpandit2011@gmail.com>
2018-09-05 23:50:19 +0530

    Java Calc

--- java/hello-world/calculator/BasicCal.java
+++ java/hello-world/calculator/BasicCal.java
@@ -1,17 +1,8 @@
 public class BasicCal{
     public static void main(String[] args) {
-        System.out.println(sum(2,3));
-        System.out.println(mul(2,3));
-        System.out.println(sub(2,3));
-        int sum(int a, int b){
-            return a+b;
-        }
-        int mul(int a, int b){
-            return a*b;
-        }
-        int sub(int a, int b){
-            return a-b;
-        }
+        System.out.println(2+3);
+        System.out.println(2*3);
+        System.out.println(2-3);
     }
     
 }
