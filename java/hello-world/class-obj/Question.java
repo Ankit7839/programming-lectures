1d7d5cf8d49f7692af3f30ce3dedc86a28663fb4
git_chetan <chetanpandit2011@gmail.com>
2018-09-07 13:34:31 +0530

    Class and Object.

---
+++ java/hello-world/class-obj/Question.java
@@ -0,0 +1,9 @@
+import jdk.nashorn.internal.ir.ReturnNode;
+
+public class Question{
+    int var1, var2;
+    int sum(int val1, int val2){
+        int res = val1+val2;
+        return res;
+    }
+}
