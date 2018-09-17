dcef9d1a07dff32a340fd742cd52b016c110794a
git_chetan <chetanpandit2011@gmail.com>
2018-09-17 23:12:36 +0530

    For of loop in Javascript

--- webtech/14-javascript-maps/maps.js
+++ webtech/14-javascript-maps/maps.js
@@ -23,5 +23,21 @@ users.set("first", Chetan);
 users.set("second", Mayur);
 users.set("third", Gunjan);
 
-console.log(users);
+// console.log(users);
+// console.log(users.size);
+// console.log(users.get("first"))
 
+// console.log(users.keys());
+// console.log(users.values());
+
+// for(const key of users.keys()){
+//     console.log(key);
+//     console.log(users.get(key));
+//     console.log(users.get(key).name);
+    
+// }
+
+for (const [key, value] of users.entries()) {
+    console.log(key +" : " + value.name);
+    
+    }
